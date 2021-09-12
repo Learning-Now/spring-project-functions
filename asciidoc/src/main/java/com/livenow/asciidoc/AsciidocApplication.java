2f30049fa8e8bc9cd7f59054d30784fde3eab42e
livenow14 <xowjd41@naver.com>
2021-09-12 21:45:33 +0900

    Initialize commit

---
+++ asciidoc/src/main/java/com/livenow/asciidoc/AsciidocApplication.java
@@ -0,0 +1,13 @@
+package com.livenow.asciidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsciidocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsciidocApplication.class, args);
+    }
+
+}
---
+++ asciidoc/src/test/java/com/livenow/asciidoc/AsciidocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livenow.asciidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsciidocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
